733c3bfabaf2244c3ff22d172f6d8bad1bd713d8
DanielM <danieljhonsend99@gmail.com>
2020-12-26 20:10:00 -0500

    add new form and scrollreveal js

---
+++ main.js
@@ -0,0 +1,11 @@
+ScrollReveal().reveal('.main-left', {delay: 300});
+
+ScrollReveal().reveal('.container-card', {delay: 300});
+
+ScrollReveal().reveal('.intro-equipo', {delay: 300});
+ScrollReveal().reveal('.box', {delay: 300});
+
+ScrollReveal().reveal('.form', {delay: 300});
+ScrollReveal().reveal('.text-left', {delay: 300});
+
+ScrollReveal().reveal('.clientes', {delay: 300});
